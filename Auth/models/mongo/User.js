25e970a8890c9218c61ad48647098194159b450a
DimaCrafter <DimaCrafter@users.noreply.github.com>
2020-05-22 19:15:58 +0400

    Update User.js

--- Auth/models/mongo/User.js
+++ Auth/models/mongo/User.js
@@ -1,5 +1,5 @@
 module.exports = {
     firstname: { type: String, required: true },
     login: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }
 };
